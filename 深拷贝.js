6db876917bb8803bb5b8c655df9e8a8296e7947a
panle666 <1093683149@qq.com>
2022-10-06 21:22:08 +0800

    深拷贝

--- 深拷贝.js
+++ 深拷贝.js
@@ -1,7 +1,25 @@
 //由于引用类型赋值只会复制栈空间的引用地址，内容一旦修改会影响其它变量，故而要实现深拷贝，将引用关系分离
 const obj1 = { name: 'a' };
 
-const obj2 = structuredClone(obj1);
+function deepClone(initalObj, finalObj) {
+    var obj = finalObj || {};
+    for (var i in initalObj) {
+        var prop = initalObj[i];        // 避免相互引用对象导致死循环，如initalObj.a = initalObj的情况
+        if (prop === obj) {
+            continue;
+        }
+        if (typeof prop === 'object') {
+            obj[i] = (prop.constructor === Array) ? [] : {};
+            arguments.callee(prop, obj[i]);
+        } else {
+            obj[i] = prop;
+        }
+    }
+    return obj;
+}
+
+// const obj2 = structuredClone(obj1);
+const obj2 = deepClone(obj1);
 
 obj2.name = 'b';
 console.log(obj1.name);// a
