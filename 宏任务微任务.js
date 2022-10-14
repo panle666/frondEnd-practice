bb773f3d0f08bd73698f8fa3f53468c54aecf3c3
panle666 <1093683149@qq.com>
2022-10-14 16:30:31 +0800

    宏任务微任务调整

--- 宏任务微任务.js
+++ 宏任务微任务.js
@@ -1,18 +1,28 @@
-//先执行微任务，再执行宏任务
+//先执行宏任务，再执行微任务
 
 //web:
 //微任务：MutationObserver、Promise.then catch finally
-//宏任务： I/O、setTimeout、setInterval、requestAnimationFrame
+//宏任务： I/O、setTimeout、setInterval、requestAnimationFrame、script
 
+//任务：同步任务
+//     异步任务：宏任务、微任务
 
-setTimeout(_ => console.log(4));
+// 当前script作为第一个宏任务先执行，然后依次执行里面的同步任务、微任务
+// settimeout作为第二个宏任务放入宏任务队列，先执行完第一个宏任务script的微任务：Promise.then()
+// 最后执行第二个宏任务setTimeout
+
+//<script>
+
+setTimeout(_ => console.log(4));//宏任务，先执行完宏任务script下的微任务，最后再执行console.log(4)
 
 new Promise(resolve => {
     resolve()
-    console.log(1);
+    console.log(1);//同步任务
 }).then(_ => {
-    console.log(3);
+    console.log(3);//script下的微任务
 })
 
-console.log(2); // 1 2 3 4
+console.log(2); //同步任务
 
+//打印结果： 1 2 3 4
+//</script>
